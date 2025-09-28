2aed3d0c25408d51aafd2498452bc502697c48ac
BuiAnh12 <buianh120403@gmail.com>
2025-09-28 16:10:25 +0700

    FEAT: Fav API and User API

---
+++ controllers/user.controller.js
@@ -0,0 +1,22 @@
+const { getUserService, updateUserService } = require("../services/user.service");
+const ApiResponse = require("../utils/ApiResponse");
+
+const getUser = async (req, res) => {
+  try {
+    const user = await getUserService(req.params.id);
+    return ApiResponse.success(res, user, "User fetched successfully");
+  } catch (err) {
+    return ApiResponse.error(res, err);
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const user = await updateUserService(req.user._id, req.body);
+    return ApiResponse.success(res, user, "User updated successfully");
+  } catch (err) {
+    return ApiResponse.error(res, err);
+  }
+};
+
+module.exports = { getUser, updateUser };
